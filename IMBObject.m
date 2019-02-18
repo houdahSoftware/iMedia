20bc90b1faefddd5d7a8d92e516e037d29506e75
Daniel Jalkut <jalkut@red-sweater.com>
2019-02-18 11:19:13 -0500

    Consolidate IMBObject "thumbnail" method for generating an NSImage with IMBImageItem convenience method for achieving the same thing, so we can have a single bottleneck for generating NSImage representation of IMBObject.

--- IMBObject.m
+++ IMBObject.m
@@ -392,26 +392,21 @@ NSString* kIMBObjectPasteboardType = @"com.karelia.imedia.IMBObject";
  */
 - (NSImage *)thumbnail
 {
-    if ([self imageRepresentation] == nil) {
-        return nil;
-    }
-    
-    if ([self.imageRepresentationType isEqualToString:IKImageBrowserNSImageRepresentationType])
-    {
-        return (NSImage *) self.imageRepresentation;
-    }
-    
-    if ([self.imageRepresentationType isEqualToString:IKImageBrowserCGImageRepresentationType])
-    {
-        return [[NSImage alloc] initWithCGImage:(CGImageRef) self.imageRepresentation size:CGSizeZero];
-    }
-    
-    if ([self.imageRepresentationType isEqualToString:IKImageBrowserNSDataRepresentationType])
-    {
-        return [[NSImage alloc] initWithData:(NSData *) self.imageRepresentation];
-    }
-    
-    return self.icon;
+	NSImage* result = nil;
+
+	// While we haven't loaded an image representation yet, faithfully report that
+    if ([self imageRepresentation] != nil)
+	{
+		result = [NSImage imageWithIMBImageItem:self];
+
+		// Last resort: use the item's icon
+		if (result == nil)
+		{
+			result = self.icon;
+		}
+	}
+
+	return result;
 }
 
 
