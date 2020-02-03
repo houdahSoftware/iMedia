cba314fd0f297c2d8ef91a4692bea3a7b82e20d8
gloubibou <pierre.bernard@web.de>
2020-02-03 11:56:23 +0100

    Bind selectionIndexPaths instead of selectionIndexes. This works around an issue where the icon view scrolls to the top upon clicking an item to select it

---
+++ IMBObjectCollectionViewIndexPathTransformer.h
@@ -0,0 +1,15 @@
+//
+//  IMBObjectCollectionViewIndexPathTransformer.h
+//  iMedia
+//
+//  Created by Pierre Bernard on 03.02.20.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface IMBObjectCollectionViewIndexPathTransformer : NSValueTransformer
+
+- (instancetype)init;
+
+@end
