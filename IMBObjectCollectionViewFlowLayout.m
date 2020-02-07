54dbaa29b8fecca01e6a88e1f2eb35c758ec7b23
gloubibou <pierre.bernard@web.de>
2020-02-07 12:30:45 +0100

    Allow right and left arrow key selection to wrap to the next / previous line

--- IMBObjectCollectionViewFlowLayout.m
+++ IMBObjectCollectionViewFlowLayout.m
@@ -13,6 +13,15 @@
 @end
 
 
+@interface NSCollectionViewLayout (Private)
+
+- (NSCollectionViewLayoutAttributes *)layoutAttributesForNextItemInDirection:(NSPoint)direction
+															   fromIndexPath:(NSIndexPath *)indexPath
+														   constrainedToRect:(NSRect)rect;
+
+@end
+
+
 @implementation IMBObjectCollectionViewFlowLayout
 
 // NOTE: Apple private API
@@ -20,6 +29,71 @@
 {
 	// Enables shift-click range selections
 	return YES;
+}
+
+// NOTE: Apple private API
+- (NSCollectionViewLayoutAttributes *)layoutAttributesForNextItemInDirection:(NSPoint)direction
+															   fromIndexPath:(NSIndexPath *)indexPath
+														   constrainedToRect:(NSRect)rect
+{
+	// Allows right and left arrow key selection to wrap to the next / previous line
+	if (direction.y == 0) {
+		NSCollectionView *collectionView = self.collectionView;
+
+		if (collectionView != nil) {
+			NSCollectionViewLayoutAttributes *result = [super layoutAttributesForNextItemInDirection:direction
+																					   fromIndexPath:indexPath
+																				   constrainedToRect:[collectionView bounds]];
+
+			if (result != nil) {
+				return result;
+			}
+
+			NSUInteger numberOfSections = [collectionView numberOfSections];
+
+			// hit edge of view
+			NSInteger section = -1;
+			NSInteger item = - 1;
+
+			if (direction.x == 1) {
+				// right
+				section = indexPath.section;
+				item = indexPath.item + 1;
+
+				while ((section < numberOfSections) && (item >= [collectionView numberOfItemsInSection:section])) {
+					item = 0;
+					section += 1;
+				}
+			}
+			else if (direction.x == -1) {
+				// left
+				section = indexPath.section;
+				item = indexPath.item - 1;
+
+				while (item < 0) {
+					section -= 1;
+
+					if (section < 0) {
+						break;
+					}
+
+					item = [collectionView numberOfItemsInSection:section] - 1;
+				}
+			}
+
+			if ((section > -1) && (item > -1) && (section < numberOfSections) && (item < [collectionView numberOfItemsInSection:section])) {
+				NSIndexPath *nextIndexPath = [NSIndexPath indexPathForItem:item inSection:section];
+
+				return [self layoutAttributesForItemAtIndexPath:nextIndexPath];
+			}
+		}
+	}
+
+	NSCollectionViewLayoutAttributes *result = [super layoutAttributesForNextItemInDirection:direction
+																			   fromIndexPath:indexPath
+																		   constrainedToRect:rect];
+
+	return result;
 }
 
 @end
