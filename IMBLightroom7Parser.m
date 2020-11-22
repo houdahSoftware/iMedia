cfc1ad6cef44e733889f792c69b27b8fbca273ee
gloubibou <pierre.bernard@web.de>
2020-11-22 18:07:28 +0100

    Compatibility with Lightroom Classic CC

--- IMBLightroom7Parser.m
+++ IMBLightroom7Parser.m
@@ -128,7 +128,7 @@
 		if (databaseVersionLong < 700003) {
 			return NO;
 		}
-		else if (databaseVersionLong >= 1000000) {
+		else if (databaseVersionLong >= 1100000) {
 			return NO;
 		}
         
